2f4a9f134faad6730b38a4a5fd7eb3295900ecf1
IAbrahanarley <abrahanarley96@gmail.com>
2023-09-05 12:31:48 -0300

    bug fix e inicio da seçao habilidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-window.addEventListener('scroll', function() {
-    const progressBar = document.querySelector('.bareffect');
-    progressBar.classList.add('bareffect');
-    const rect = progressBar.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-});
-// Botão de voltar ao topo
 const btnScrollToTop = document.getElementById("btnScrollToTop");
 
 // Adicione um ouvinte de evento de rolagem para mostrar ou ocultar o botão
