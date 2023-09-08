2468c2ae90713e29fafa00f786a50018f2694ee9
IAbrahanarley <abrahanarley96@gmail.com>
2023-09-07 21:33:24 -0300

    finalizado seçao habilidades e ajustes na seçao home e nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 const btnScrollToTop = document.getElementById("btnScrollToTop");
 
-// Adicione um ouvinte de evento de rolagem para mostrar ou ocultar o botão
 window.addEventListener("scroll", () => {
     if (window.scrollY > 300) {
         btnScrollToTop.style.display = "block";
@@ -9,10 +8,46 @@ window.addEventListener("scroll", () => {
     }
 });
 
-// Adicione um ouvinte de evento de clique para rolar suavemente de volta ao topo
 btnScrollToTop.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
 });
+
+// efeito de escrita
+const textoElemento = document.getElementById('texto');
+const frases = ["Desenvolvedor Web", "Backend."];
+let indiceFrase = 0;
+let indiceCaractere = 0;
+let escrevendo = true;
+let timeout;
+
+function escreverApagarTexto() {
+    const fraseAtual = frases[indiceFrase];
+
+    if (escrevendo) {
+        textoElemento.textContent = fraseAtual.slice(0, indiceCaractere);
+        indiceCaractere++;
+
+        if (indiceCaractere > fraseAtual.length) {
+            escrevendo = false;
+            timeout = setTimeout(escreverApagarTexto, 400);
+        } else {
+            timeout = setTimeout(escreverApagarTexto, 60); 
+        }
+    } else {
+        textoElemento.textContent = fraseAtual.slice(0, indiceCaractere);
+        indiceCaractere--;
+
+        if (indiceCaractere === 0) {
+            escrevendo = true;
+            indiceFrase = (indiceFrase + 1) % frases.length;
+            timeout = setTimeout(escreverApagarTexto, 400); 
+        } else {
+            timeout = setTimeout(escreverApagarTexto, 60); 
+        }
+    }
+}
+
+escreverApagarTexto(); 
