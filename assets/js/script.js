ea086a7c1ca67d816e74050d49b11d0b900dbf7b
IAbrahanarley <abrahanarley96@gmail.com>
2023-09-09 10:33:33 -0300

    finalizado seçao skills e inicio seçao projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ btnScrollToTop.addEventListener("click", () => {
 
 // efeito de escrita
 const textoElemento = document.getElementById('texto');
-const frases = ["Desenvolvedor Web", "Backend."];
+const frases = ["Web Developer", "Backend."];
 let indiceFrase = 0;
 let indiceCaractere = 0;
 let escrevendo = true;
@@ -34,7 +34,7 @@ function escreverApagarTexto() {
             escrevendo = false;
             timeout = setTimeout(escreverApagarTexto, 400);
         } else {
-            timeout = setTimeout(escreverApagarTexto, 60); 
+            timeout = setTimeout(escreverApagarTexto, 60);
         }
     } else {
         textoElemento.textContent = fraseAtual.slice(0, indiceCaractere);
@@ -43,11 +43,58 @@ function escreverApagarTexto() {
         if (indiceCaractere === 0) {
             escrevendo = true;
             indiceFrase = (indiceFrase + 1) % frases.length;
-            timeout = setTimeout(escreverApagarTexto, 400); 
+            timeout = setTimeout(escreverApagarTexto, 400);
         } else {
-            timeout = setTimeout(escreverApagarTexto, 60); 
+            timeout = setTimeout(escreverApagarTexto, 60);
         }
     }
 }
 
-escreverApagarTexto(); 
+escreverApagarTexto();
+
+// efeito mudar cor do menu
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.menu');
+    if (window.scrollY > 50) { // A altura em que deseja que a cor da navbar mude
+        navbar.style.backgroundColor = 'white';
+        navbar.classList.add('shadow');
+    } else {
+        navbar.style.backgroundColor = 'transparent'; // Cor da barra de navegação quando no topo
+        navbar.classList.remove('shadow');
+    }
+});
+
+// carrossel
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
+let current = 0;
+let prev = 5;
+let next = 1;
+
+for(let i = 0; i < button.length; i++){
+    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
+const gotoPrev = () => current > 0 ? gotoNum(current -1) : gotoNum(slides.length -1);
+const gotoNext = () => current < 5 ? gotoNum(current +1) : gotoNum(0);
+const gotoNum = number => {
+    current = number;
+    prev = current - 1;
+    next = current + 1;
+
+    for(let i = 0; i < slides.length; i++){
+        slides[i].classList.remove("active");
+        slides[i].classList.remove("prev");
+        slides[i].classList.remove("next");
+    }
+    if(next == 0){
+        next = 0;
+    }
+    if(prev == -1){
+        prev = 5;
+    }
+    slides[current].classList.add("active");
+    slides[prev].classList.add("prev");
+    slides[next].classList.add("next");
+}
