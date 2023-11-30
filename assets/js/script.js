24652a3baef5404541245ef1a185fd25df6a645d
IAbrahanArley <abrahanarley96@gmail.com>
2023-11-30 11:57:28 -0300

    ajustes de responsividade, carrosel e paleta de cores

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,39 +66,16 @@ window.addEventListener('scroll', () => {
     }
 });
 
-// carrossel
-const slider = document.querySelector(".items");
-const slides = document.querySelectorAll(".item");
-const button = document.querySelectorAll(".button");
-const card = document.querySelectorAll("card")
-let current = 0;
-let prev = 5;
-let next = 1;
 
-for(let i = 0; i < button.length; i++){
-    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-}
-const gotoPrev = () => current > 0 ? gotoNum(current -1) : gotoNum(slides.length -1);
-const gotoNext = () => current < 5 ? gotoNum(current +1) : gotoNum(0);
-const gotoNum = number => {
-    current = number;
-    prev = current - 1;
-    next = current + 1;
 
-    for(let i = 0; i < slides.length; i++){
-        slides[i].classList.remove("active");
-        slides[i].classList.remove("card");
-        slides[i].classList.remove("prev");
-        slides[i].classList.remove("next");
-    }
-    if(next == 0){
-        next = 0;
-    }
-    if(prev == -1){
-        prev = 5;
-    }
-    slides[current].classList.add("active");
-    slides[current].classList.add("card")
-    slides[prev].classList.add("prev");
-    slides[next].classList.add("next");
+// carrosel 
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    e.target.matches('.next') && slider.append(items[0])
+    e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
 }
+
+document.addEventListener('click', activate, false);
